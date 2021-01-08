61b70819701cf8aab92428ae24e8577700a2eda4
shay-m <shymor0@gmail.com>
2021-01-08 13:00:46 -0500

    Updated flask

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, Response, request, jsonify
-from werkzeug.utils import secure_filenameimport numpy as np
+from werkzeug.utils import secure_filename
+import numpy as np
 import cv2
 
 app = Flask(__name__)
 
 @app.route('/image', methods=['POST'])
 def image():
-    print(request.files)
     file = request.files['file']
     filename = secure_filename(file.filename)
     file.save(filename)
